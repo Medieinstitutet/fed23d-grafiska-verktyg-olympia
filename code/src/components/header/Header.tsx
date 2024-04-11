9e9d6ce5aa3f7668d2666b1f0d7cf0f6a39b2d84
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-11 14:01:01 +0200

    :alembic: adds test divider

--- code/src/components/header/Header.tsx
+++ code/src/components/header/Header.tsx
@@ -7,18 +7,20 @@ const Header = () => {
         <div>            
             <LogoComponent />
         </div>
-        <div>Här är en divider</div>
+        <div className="test-divider"></div>
         <nav> 
-            <h1>
+            <h1 className="menu">
                 Recipes FAQ About Contact
             </h1>
         </nav>
-        <div>Här är en divider</div>
+        <div className="test-divider"></div>
         <HeaderImageComponent />
-        <div>Här är en divider</div>
+        <div className="test-divider"></div>
     </header> 
     //Nav kommer in som separat komponent 
     //Divider kommer in som separat komponent
+
+    //Ändra header image vid mindre än tablet
   )
 }
 
