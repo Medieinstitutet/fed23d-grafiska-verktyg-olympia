9d02d29ae2a813b36518de87dd835a88beba8912
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-11 15:58:57 +0200

    :construction: adds context, and desktop image. work in progress

--- code/src/components/header/HeaderImage.tsx
+++ code/src/components/header/HeaderImage.tsx
@@ -1,8 +1,18 @@
 import HeaderImage from '../../assets/images/headerimage-tab-mob.jpg'
+import HeaderImageDesktop from '../../assets/images/headerbread.jpg';
+import Context from '../../context/Context'; 
+import { useContext } from 'react';
 
 const HeaderImageComponent = () => {
+    const context = useContext(Context);
+    if (!context) return; 
+
+    const { isMobile } = context; 
+
     return (
-        <img className="header-image" src={HeaderImage} alt="Heading photo of sourdough bread" width="295" height="211" /> 
+        <div className="header-image-container">
+            <img className="header-image" src={isMobile ? HeaderImage : HeaderImageDesktop} alt="Heading photo of sourdough bread" width="295" height="211" /> 
+        </div>    
     );
   };
   
