d03cdc85089e40b17ea760c74a6c46e8a3f5620e
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-11 16:00:05 +0200

    :recycle: removes space

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,11 +1,10 @@
-import Header from "./components/header/Header"
+import Header from "./components/header/Header";
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
-
       <Footer />
     </>
   );
