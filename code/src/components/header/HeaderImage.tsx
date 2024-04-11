fe3110d03aff5860ba72a806a296789e44e3b242
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-11 11:31:08 +0200

    :camera_flash: adds header image for tablet and mobile

--- code/src/components/header/HeaderImage.tsx
+++ code/src/components/header/HeaderImage.tsx
@@ -1,8 +1,8 @@
-import HeaderImage from '../../assets/images/headerbread.jpg'
+import HeaderImage from '../../assets/images/headerimage-tab-mob.jpg'
 
 const HeaderImageComponent = () => {
     return (
-        <img className="header-image" src={HeaderImage} alt="Heading photo of sourdough bread" width="798" height="305" /> 
+        <img className="header-image" src={HeaderImage} alt="Heading photo of sourdough bread" width="295" height="211" /> 
     );
   };
   
