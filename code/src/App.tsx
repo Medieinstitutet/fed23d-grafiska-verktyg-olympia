a31acf97559da4626bbaad2dac9a279a8270ff68
RalfiSlask <nilssonnarki@gmail.com>
2024-04-12 07:45:46 +0200

    :sparkles: import Cookiebar and isCookiebarOpen state and use and or operator for its visibility

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,9 +1,19 @@
+import Cookiebar from './components/cookiebar/Cookiebar';
 import Footer from './components/footer/Footer';
+import { useContext } from 'react';
+import Context from './context/Context';
 
 function App() {
+  const context = useContext(Context);
+
+  if (!context) return;
+
+  const { isCookiebarOpen } = context;
+
   return (
     <>
       <Footer />
+      {isCookiebarOpen && <Cookiebar />}
     </>
   );
 }
