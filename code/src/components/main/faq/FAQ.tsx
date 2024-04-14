5e3864612c8f48e774cda65610dfaaa37530ffbc
RalfiSlask <nilssonnarki@gmail.com>
2024-04-14 09:20:20 +0200

    :sparkles: add FAQ component where i map over questioncontainer for questiona and answers

---
+++ code/src/components/main/faq/FAQ.tsx
@@ -0,0 +1,24 @@
+import Heading from '../../shared/Heading';
+import { questionsAndAnswers } from '../../../data/Texts';
+import QuestionContainer from './QuestionContainer';
+import { v4 as uuidv4 } from 'uuid';
+import Quote from '../../shared/Quote';
+import SubscriptionInput from './SubscriptionInput';
+
+const FAQ = () => {
+  return (
+    <section className="faq-container">
+      <Quote />
+      <SubscriptionInput />
+      <Heading title="FAQ" />
+      <div className="faq-wrapper">
+        {questionsAndAnswers.map(faq => {
+          const { answer, question } = faq;
+          return <QuestionContainer key={uuidv4()} text={answer} question={question} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default FAQ;
